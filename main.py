ddf91471927c35d6d98ece91e13832a933d7c908
Scooter76000 <33722779+scooter76000@users.noreply.github.com>
2023-02-03 21:24:01 +0000

    nettoyage

--- main.py
+++ main.py
@@ -1,69 +1,21 @@
 import csv
 
-data_csv = "fichier1.csv"
+# ------ chemin des fichier.csv  ------
+data_csv = "2020_installations_sportives.csv" # fichier orginal (sujet)
 
-"""data_csv = "/home/runner/Projet-Data-Groupe-G/fichier1.csv" #chemin fichier selon le pwd 
+# Fichier constituer du code de l'installation, nom de l'installation, nombre d'Equipement, de l'adresse et du nombre de place de parking handicaper ou pas.
+data_installations_uniques_csv = "data/IntallationsUniques.csv" 
 
+#Fichier constituer du code INSEE de la commune, du nom de la commune, du code du department et du nom du département.
+data_communes_uniques_csv = "data/CommunesUniques.csv"
 
-pwd est un commande linux/Unix pour avoir le nom complet de l'arborescence d'un fichier
-
-
-with open(data_csv, errors="ignore") as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for line in csv_reader:
-        print(line)
-"""
-
-"""
-simple_csv = "fichier1.csv"
-#def fichier_1 ():
-fichier_sortie1 = "installations_uniques.csv"
-colonnes = ["codeInstallation","nomInstallation","adresse","nbParking","nbParkingHand","nbEquipements"]
-with open (simple_csv, 'r') as csv_file :
-    reader = csv.DictReader(csv_file)
-    headers = reader.fieldnames 
-    if not all(col in headers for col in colonnes):
-        raise Exception("Les colonnes spécifiées ne sont pas toutes présentes dans le fichier d'entrée")
-    with open (fichier_sortie1, 'w') as csv_sortie : 
-        writer = csv.DictWriter(csv_sortie)
-        writer.writeheader()
-        for row in reader:
-            data = {col: row[col] for col in colonnes}
-            writer.writerow(data)
-"""
-
-"""
-simple_csv = "fichier1.csv"
+# Fichier constituer du code de l'installation, du code INSEE de la commune, du libelle, des different moyen de transport(Metro,Bus,Tram,Train,Bateau ou autre voir pas) et des la date de mise à jour et de création
+data_admissions_par_formation_detaillee_csv = "data/AdmissionParFormationDetaillee.csv"
 
 
-def creation_fichier(fichier_entre, nom_fichier, colonnes):
-    liste_fichier = []
-    with open(fichier_entre, 'r', errors="ignore", newline= '') as csv_file :
-      reader = csv.reader(csv_file,  delimiter=' ', quotechar='|')
-      for ligne in reader :
-        liste_fichier.append(', '.join(ligne))
-    fich_sortie = nom_fichier
-    with open(fich_sortie, 'w', newline='') as csv_sortie :
-        writer = csv.writer(csv_sortie)
-        writer.writerow([liste_fichier[i][j] for j in colonnes] for i in range(len(liste_fichier)))
-       
-        
+# ------ Fonction -------
 
-
-
-creation_fichier(simple_csv, [2], "installations_uniques.csv")
-
-"""
-
-"""
-######    Lecture / Ecriture : ok    ###### début 
-
-data_csv = "/home/runner/Projet-Data-Groupe-G/fichier1.csv" #chemin fichier selon le pwd
-
-# pwd est un commande linux/Unix pour avoir le nom complet de l'arborescence d'un fichier
-
-
-######    Lecutre fichier CSV    ######
+# ------ Fonction Lecture du fichier.csv ------
 
 # fonction lecture de fichier CSV avec en paramètres le nom du fichier.csv
 def lecture_csv(nom_fichier_csv):
@@ -76,21 +28,11 @@ def lecture_csv(nom_fichier_csv):
             # Affichage de la ligne lu
             print(line)
 
-lecture_csv(data_csv)
-
-######    Ecriture fichier CSV    ######
-
-# Fichier constituer du code de l'installation, nom de l'installation, nombre d'Equipement, de l'adresse et du nombre de place de parking handicaper ou pas.
-data_installations_uniques_csv = "/home/runner/Projet-Data-Groupe-G/data/IntallationsUniques.csv"
-
-#Fichier constituer du code INSEE de la commune, du nom de la commune, du code du department et du nom du département.
-data_communes_uniques_csv = "/home/runner/Projet-Data-Groupe-G/data/CommunesUniques.csv"
-
-# Fichier constituer du code de l'installation, du code INSEE de la commune, du libelle, des different moyen de transport(Metro,Bus,Tram,Train,Bateau ou autre voir pas) et des la date de mise à jour et de création
-data_admissions_par_formation_detaillee_csv = "/home/runner/Projet-Data-Groupe-G/data/AdmissionParFormationDetaillee.csv"
-
-
+          
+# ------   Fontion Écriture du fichier.csv   -------
+          
 # fonction écriture de fichier CSV avec en paramètre le nom du fichier.cvs et l'en tête
+
 def ecriture_csv(nom_fichier_csv, header):
     # Ouverutre d'un fichier csv en mode 'w" write = écriture et un saut de ligne
     with open(nom_fichier_csv, "w", newline="") as csv_file:
@@ -101,26 +43,8 @@ def ecriture_csv(nom_fichier_csv, header):
         # Écriture du fichier.csv
         writer.writerow(["31", "Haute-Garonne"]) # test ecriture dans le fichier csv
 
-header_installations_uniques_csv =["codeInstallation", "nomInstallation", "nbEquipements", "adresse", "nbParking", "nbParkingHandi"]
-ecriture_csv(data_installations_uniques_csv, header_installations_uniques_csv)
 
-header_communes_uniques_csv = ["codeInseeCommune", "nomCommune", "codeDepartement", "nomDepartement"]
-ecriture_csv(data_communes_uniques_csv, header_communes_uniques_csv)
-
-header_admissions_par_formation_detaillee_csv = ["codeInstallation", "codeInseeCommune", "libelle", "metro", "bus", "tram", "train", "bateau", "autreTransport", "aucunTransport", "dateMAJ", "dateCreation"]
-ecriture_csv(data_admissions_par_formation_detaillee_csv, header_admissions_par_formation_detaillee_csv)
-
-######   Test des nouveau fichier csv cree   #####
-
-lecture_csv(data_installations_uniques_csv)
-print("")
-lecture_csv(data_communes_uniques_csv)
-print("")
-lecture_csv(data_admissions_par_formation_detaillee_csv)
-
-######    Lecture / Ecriture : ok    ###### Fin
-
-"""
+# ----   fichier.csv en liste  --------
 
 def csv_en_liste(nom_fichier_csv, valeur_ligne, valeur_valeur):
     with open(nom_fichier_csv, errors="ignore") as f:
@@ -133,6 +57,24 @@ def csv_en_liste(nom_fichier_csv, valeur_ligne, valeur_valeur):
     #print("")
     print(f"La ligne", valeur_ligne, "du fichier qui donne", lignes[0][valeur_valeur], ":", lignes[valeur_ligne][valeur_valeur])
 
-ligne = 2
-valeur = 3
+
+
+# -----  Test des Fontion et Affichage ----
+
+# Test de la fonction lecutre_csv
+lecture_csv(data_csv)
+  
+######   Test des nouveau fichier csv cree   #####
+
+lecture_csv(data_installations_uniques_csv)
+print("")
+lecture_csv(data_communes_uniques_csv)
+print("")
+lecture_csv(data_admissions_par_formation_detaillee_csv)
+
+  
+# ----  Test de la fonction fichier.csv en liste ----
+
+ligne = 2 # ligne 0 = en-tête 
+valeur = 3 # valeur = A sur le execel 
 csv_en_liste(data_csv, ligne, valeur)
---
+++ main_test.py
@@ -0,0 +1,120 @@
+
+"""data_csv = "/home/runner/Projet-Data-Groupe-G/fichier1.csv" #chemin fichier selon le pwd 
+
+
+pwd est un commande linux/Unix pour avoir le nom complet de l'arborescence d'un fichier
+
+
+with open(data_csv, errors="ignore") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for line in csv_reader:
+        print(line)
+"""
+
+"""
+simple_csv = "fichier1.csv"
+#def fichier_1 ():
+fichier_sortie1 = "installations_uniques.csv"
+colonnes = ["codeInstallation","nomInstallation","adresse","nbParking","nbParkingHand","nbEquipements"]
+with open (simple_csv, 'r') as csv_file :
+    reader = csv.DictReader(csv_file)
+    headers = reader.fieldnames 
+    if not all(col in headers for col in colonnes):
+        raise Exception("Les colonnes spécifiées ne sont pas toutes présentes dans le fichier d'entrée")
+    with open (fichier_sortie1, 'w') as csv_sortie : 
+        writer = csv.DictWriter(csv_sortie)
+        writer.writeheader()
+        for row in reader:
+            data = {col: row[col] for col in colonnes}
+            writer.writerow(data)
+"""
+
+"""
+simple_csv = "fichier1.csv"
+
+
+def creation_fichier(fichier_entre, nom_fichier, colonnes):
+    liste_fichier = []
+    with open(fichier_entre, 'r', errors="ignore", newline= '') as csv_file :
+      reader = csv.reader(csv_file,  delimiter=' ', quotechar='|')
+      for ligne in reader :
+        liste_fichier.append(', '.join(ligne))
+    fich_sortie = nom_fichier
+    with open(fich_sortie, 'w', newline='') as csv_sortie :
+        writer = csv.writer(csv_sortie)
+        writer.writerow([liste_fichier[i][j] for j in colonnes] for i in range(len(liste_fichier)))
+       
+        
+
+
+
+creation_fichier(simple_csv, [2], "installations_uniques.csv")
+
+"""
+
+"""
+######    Lecture / Ecriture : ok    ###### début 
+
+data_csv = "/home/runner/Projet-Data-Groupe-G/fichier1.csv" #chemin fichier selon le pwd
+
+# pwd est un commande linux/Unix pour avoir le nom complet de l'arborescence d'un fichier
+
+
+######    Lecutre fichier CSV    ######
+
+# fonction lecture de fichier CSV avec en paramètres le nom du fichier.csv
+def lecture_csv(nom_fichier_csv):
+    # Ouverture d'un fichier csv avec en paramètres un mode "r" read = lecture
+    with open(nom_fichier_csv, errors="ignore") as csv_file:
+        # Lecture du fichier csv
+        csv_reader = csv.reader(csv_file)
+        # Boucle pour lire le fichier csv
+        for line in csv_reader:
+            # Affichage de la ligne lu
+            print(line)
+
+lecture_csv(data_csv)
+
+######    Ecriture fichier CSV    ######
+
+# Fichier constituer du code de l'installation, nom de l'installation, nombre d'Equipement, de l'adresse et du nombre de place de parking handicaper ou pas.
+data_installations_uniques_csv = "/home/runner/Projet-Data-Groupe-G/data/IntallationsUniques.csv"
+
+#Fichier constituer du code INSEE de la commune, du nom de la commune, du code du department et du nom du département.
+data_communes_uniques_csv = "/home/runner/Projet-Data-Groupe-G/data/CommunesUniques.csv"
+
+# Fichier constituer du code de l'installation, du code INSEE de la commune, du libelle, des different moyen de transport(Metro,Bus,Tram,Train,Bateau ou autre voir pas) et des la date de mise à jour et de création
+data_admissions_par_formation_detaillee_csv = "/home/runner/Projet-Data-Groupe-G/data/AdmissionParFormationDetaillee.csv"
+
+
+# fonction écriture de fichier CSV avec en paramètre le nom du fichier.cvs et l'en tête
+def ecriture_csv(nom_fichier_csv, header):
+    # Ouverutre d'un fichier csv en mode 'w" write = écriture et un saut de ligne
+    with open(nom_fichier_csv, "w", newline="") as csv_file:
+        # Écriture du fichier csv avec un paramètre un delimitation par le ";"
+        writer = csv.writer(csv_file, delimiter=";")
+        # Écriture de l'en tête du fichier csv
+        writer.writerow(header)
+        # Écriture du fichier.csv
+        writer.writerow(["31", "Haute-Garonne"]) # test ecriture dans le fichier csv
+
+header_installations_uniques_csv =["codeInstallation", "nomInstallation", "nbEquipements", "adresse", "nbParking", "nbParkingHandi"]
+ecriture_csv(data_installations_uniques_csv, header_installations_uniques_csv)
+
+header_communes_uniques_csv = ["codeInseeCommune", "nomCommune", "codeDepartement", "nomDepartement"]
+ecriture_csv(data_communes_uniques_csv, header_communes_uniques_csv)
+
+header_admissions_par_formation_detaillee_csv = ["codeInstallation", "codeInseeCommune", "libelle", "metro", "bus", "tram", "train", "bateau", "autreTransport", "aucunTransport", "dateMAJ", "dateCreation"]
+ecriture_csv(data_admissions_par_formation_detaillee_csv, header_admissions_par_formation_detaillee_csv)
+
+######   Test des nouveau fichier csv cree   #####
+
+lecture_csv(data_installations_uniques_csv)
+print("")
+lecture_csv(data_communes_uniques_csv)
+print("")
+lecture_csv(data_admissions_par_formation_detaillee_csv)
+
+######    Lecture / Ecriture : ok    ###### Fin
+
+"""
