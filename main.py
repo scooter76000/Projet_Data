5e8e7b568b779494326db3c415d8db96605688c8
Scooter76000 <33722779+scooter76000@users.noreply.github.com>
2023-02-04 11:58:26 +0000

    attention il y a que main qui compil

--- main.py
+++ main.py
@@ -15,9 +15,35 @@ data_admissions_par_formation_detaillee_csv = "data/AdmissionParFormationDetaill
 #  ------ Diconnaire de recherche ------
 
 dico = {
-    "a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9, "k": 10,
-        "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18, "t": 19, "u" :20,
-        "v": 21, "w": 22, "x": 23, "y": 24, "z": 25, "aa": 26, "ab": 27, "ac": 28
+  "a": 0,
+  "b": 1,
+  "c": 2,
+  "d": 3,
+  "e": 4,
+  "f": 5,
+  "g": 6,
+  "h": 7,
+  "i": 8,
+  "j": 9,
+  "k": 10,
+  "l": 11,
+  "m": 12,
+  "n": 13,
+  "o": 14,
+  "p": 15,
+  "q": 16,
+  "r": 17,
+  "s": 18,
+  "t": 19,
+  "u": 20,
+  "v": 21,
+  "w": 22,
+  "x": 23,
+  "y": 24,
+  "z": 25,
+  "aa": 26,
+  "ab": 27,
+  "ac": 28
 }
 
 # ---- requete de recherche dans le dico ----
@@ -52,66 +78,57 @@ dateMAJ = dico.get("aa")
 dateCreation = dico.get("ab")
 nbEquipements = dico.get("ac")
 
-
 # ------ Fonction -------
 
 # ----- Fonction esthétique / grap -----
 
+
 # fonction espace
 def space_graph(valeur):
-    for i in range(valeur):
-        print("")
+  for i in range(valeur):
+    print("")
+
 
 # ------ Fonction Lecture du fichier.csv ------
 
+
 # fonction lecture de fichier CSV avec en paramètres le nom du fichier.csv
 def lecture_csv(nom_fichier_csv):
-    # Ouverture d'un fichier csv avec en paramètres un mode "r" read = lecture
-    with open(nom_fichier_csv, errors="ignore") as csv_file:
-        # Lecture du fichier csv
-        csv_reader = csv.reader(csv_file)
-        # Boucle pour lire le fichier csv
-        for line in csv_reader:
-            # Affichage de la ligne lu
-            print(line)
+  # Ouverture d'un fichier csv avec en paramètres un mode "r" read = lecture
+  with open(nom_fichier_csv, errors="ignore") as csv_file:
+    # Lecture du fichier csv
+    csv_reader = csv.reader(csv_file)
+    # Boucle pour lire le fichier csv
+    for line in csv_reader:
+      # Affichage de la ligne lu
+      print(line)
 
 
 # ------   Fontion Écriture du fichier.csv   -------
 
 # fonction écriture de fichier CSV avec en paramètre le nom du fichier.cvs et l'en tête
 
+
 def ecriture_csv(nom_fichier_csv, header, data):
-    # Ouverutre d'un fichier csv en mode 'w" write = écriture et un saut de ligne
-    with open(nom_fichier_csv, "w", newline="") as csv_file:
-        # Écriture du fichier csv avec un paramètre un delimitation par le ";"
-        writer = csv.writer(csv_file, delimiter=";")
-        # Écriture de l'en tête du fichier csv
-        writer.writerow(header)
-        # Écriture du fichier.csv
-        writer.writerow(data)  # test ecriture dans le fichier csv
+  # Ouverutre d'un fichier csv en mode 'w" write = écriture et un saut de ligne
+  with open(nom_fichier_csv, "w", newline="") as csv_file:
+    # Écriture du fichier csv avec un paramètre un delimitation par le ";"
+    writer = csv.writer(csv_file, delimiter=";")
+    # Écriture de l'en tête du fichier csv
+    writer.writerow(header)
+    # Écriture du fichier.csv
+    writer.writerow(data)  # test ecriture dans le fichier csv
 
 
 # ----   fichier.csv en liste  --------
 
-def csv_en_liste(nom_fichier_csv, valeur_ligne, valeur_valeur):
-    with open(nom_fichier_csv, errors="ignore") as f:
-        lecture = csv.reader(f, delimiter=";")
-        lignes = list(lecture)
-
-    print(f"La ligne", valeur_ligne, "du fichier qui donne", lignes[0][valeur_valeur], ":",
-          lignes[valeur_ligne][valeur_valeur])
-
-    # ---- Test interne de la fonction ----
-    # print(f"La ligne 0 du fichier: {lignes[0]}.")
-    # print(f"La ligne 1 du fichier: {lignes[1]}.")
-    # print(f"Le nombre de lignes du fichier: {len(lignes)}.")
 
 def csv_en_liste_v2(nom_fichier_csv, valeur_ligne, valeur_valeur):
-    with open(nom_fichier_csv, errors="ignore") as f:
-        lecture = csv.reader(f, delimiter=";")
-        lignes = list(lecture)
+  with open(nom_fichier_csv, errors="ignore") as f:
+    lecture = csv.reader(f, delimiter=";")
+    lignes = list(lecture)
 
-    return lignes[valeur_ligne][valeur_valeur]
+  return lignes[valeur_ligne][valeur_valeur]
 
 
 # -----  Test des Fontion et Affichage ----
@@ -123,69 +140,90 @@ space_graph(4)
 ######   Test des nouveau fichier csv cree   #####
 
 #Position des en-tête codeInstallation = 4, nomInstallation = 5, nbEquipements = 28, adresse = 6, nbParking = 16, nbParkingHandi = 17
-#header_installations_uniques_csv_v2 = [(csv_en_liste_v2(data_csv, 0, 4)), (csv_en_liste_v2(data_csv, 0, 5)), (csv_en_liste_v2(data_csv, 0, 28)), (csv_en_liste_v2(data_csv, 0, 6)), (csv_en_liste_v2(data_csv, 0, 16)), (csv_en_liste_v2(data_csv, 0, 17))]
-#data_installations_uniques_csv_v2 = [(csv_en_liste_v2(data_csv, 1, 4)), (csv_en_liste_v2(data_csv, 1, 5)), (csv_en_liste_v2(data_csv, 1, 28)), (csv_en_liste_v2(data_csv, 1, 6)), (csv_en_liste_v2(data_csv, 1, 16)), (csv_en_liste_v2(data_csv, 1, 17))]
+
+# --- En-tête ---
+header_admissions_par_formation_detaillee_csv_v2_2 = [
+  (csv_en_liste_v2(data_csv, 0, codeInstallation)),
+  (csv_en_liste_v2(data_csv, 0,
+                   codeInseeCommune)), (csv_en_liste_v2(data_csv, 0, libelle)),
+  (csv_en_liste_v2(data_csv, 0, metro)), (csv_en_liste_v2(data_csv, 0, bus)),
+  (csv_en_liste_v2(data_csv, 0, tram)), (csv_en_liste_v2(data_csv, 0, train)),
+  (csv_en_liste_v2(data_csv, 0, bateau)),
+  (csv_en_liste_v2(data_csv, 0, autreTransport)),
+  (csv_en_liste_v2(data_csv, 0, aucunTransport)),
+  (csv_en_liste_v2(data_csv, 0, dateMAJ)),
+  (csv_en_liste_v2(data_csv, 0, dateCreation))
+]
+
+header_communes_uniques_csv_v2_2 = [(csv_en_liste_v2(data_csv, 0, codeInseeCommune)),(csv_en_liste_v2(data_csv, 0, nomCommune)), (csv_en_liste_v2(data_csv, 0, codeDepartement)), (csv_en_liste_v2(data_csv, 0, nomDepartement))]
 
 header_installations_uniques_csv_v2_2 = [
-  (csv_en_liste_v2(data_csv, 0, codeInstallation)), (csv_en_liste_v2(data_csv, 0, nomInstallation)),
-  (csv_en_liste_v2(data_csv, 0, nbEquipements)), (csv_en_liste_v2(data_csv, 0, adresse)),
-  (csv_en_liste_v2(data_csv, 0, nbParking)), (csv_en_liste_v2(data_csv, 0, nbParkingHandi))
-]
-data_installations_uniques_csv_v2_2 = [
-  (csv_en_liste_v2(data_csv, 1, codeInstallation)), (csv_en_liste_v2(data_csv, 1, nomInstallation)),
-  (csv_en_liste_v2(data_csv, 1, nbEquipements)), (csv_en_liste_v2(data_csv, 1, adresse)),
-  (csv_en_liste_v2(data_csv, 1, nbParking)), (csv_en_liste_v2(data_csv, 1, nbParkingHandi))
+  (csv_en_liste_v2(data_csv, 0, codeInstallation)),
+  (csv_en_liste_v2(data_csv, 0, nomInstallation)),
+  (csv_en_liste_v2(data_csv, 0, nbEquipements)),
+  (csv_en_liste_v2(data_csv, 0, adresse)),
+  (csv_en_liste_v2(data_csv, 0, nbParking)),
+  (csv_en_liste_v2(data_csv, 0, nbParkingHandi))
 ]
 
-ecriture_csv(data_installations_uniques_csv, header_installations_uniques_csv_v2_2, data_installations_uniques_csv_v2_2)
+# ---- Data ----
+data_admissions_par_formation_detaillee_csv_v2_2 = [
+  (csv_en_liste_v2(data_csv, 1, codeInstallation)),
+  (csv_en_liste_v2(data_csv, 1,
+                   codeInseeCommune)), (csv_en_liste_v2(data_csv, 1, libelle)),
+  (csv_en_liste_v2(data_csv, 1, metro)), (csv_en_liste_v2(data_csv, 1, bus)),
+  (csv_en_liste_v2(data_csv, 1, tram)), (csv_en_liste_v2(data_csv, 1, train)),
+  (csv_en_liste_v2(data_csv, 1, bateau)),
+  (csv_en_liste_v2(data_csv, 1, autreTransport)),
+  (csv_en_liste_v2(data_csv, 1, aucunTransport)),
+  (csv_en_liste_v2(data_csv, 1, dateMAJ)),
+  (csv_en_liste_v2(data_csv, 1, dateCreation))
+]
+
+data_communes_uniques_csv_v2_2 = [(csv_en_liste_v2(data_csv, 1,
+                                                   codeInseeCommune)),
+                                  (csv_en_liste_v2(data_csv, 1, nomCommune)),
+                                  (csv_en_liste_v2(data_csv, 1,
+                                                   codeDepartement)),
+                                  (csv_en_liste_v2(data_csv, 1,
+                                                   nomDepartement))]
+
+data_installations_uniques_csv_v2_2 = [
+  (csv_en_liste_v2(data_csv, 1, codeInstallation)),
+  (csv_en_liste_v2(data_csv, 1, nomInstallation)),
+  (csv_en_liste_v2(data_csv, 1, nbEquipements)),
+  (csv_en_liste_v2(data_csv, 1, adresse)),
+  (csv_en_liste_v2(data_csv, 1, nbParking)),
+  (csv_en_liste_v2(data_csv, 1, nbParkingHandi))
+]
+
+# -----  test écriture puis lecture  ------
+
+ecriture_csv(data_installations_uniques_csv,
+             header_installations_uniques_csv_v2_2,
+             data_installations_uniques_csv_v2_2)
 lecture_csv(data_installations_uniques_csv)
 space_graph(2)
 
-#Position des en-tête codeInseeCommune = 2, nomCommune = 3, codeDepartement = 0, nomDepartement = 1
-#header_communes_uniques_csv_v2 = [(csv_en_liste_v2(data_csv, 0, 2)), (csv_en_liste_v2(data_csv, 0, 3)),  (csv_en_liste_v2(data_csv, 0, 0)), (csv_en_liste_v2(data_csv, 0, 1))]
-#data_installations_uniques_csv_v2 = [(csv_en_liste_v2(data_csv, 1, 2)), (csv_en_liste_v2(data_csv, 1, 3)),  (csv_en_liste_v2(data_csv, 1, 0)), (csv_en_liste_v2(data_csv, 1, 1))]
-
-header_communes_uniques_csv_v2_2 = [
-  (csv_en_liste_v2(data_csv, 0, codeInseeCommune)), (csv_en_liste_v2(data_csv, 0, nomCommune)),
-  (csv_en_liste_v2(data_csv, 0, codeDepartement)), (csv_en_liste_v2(data_csv, 0, nomDepartement))
-]
-data_communes_uniques_csv_v2_2 = [
-  (csv_en_liste_v2(data_csv, 1, codeInseeCommune)), (csv_en_liste_v2(data_csv, 1, nomCommune)),
-  (csv_en_liste_v2(data_csv, 1, codeDepartement)), (csv_en_liste_v2(data_csv, 1, nomDepartement))
-]
-ecriture_csv(data_communes_uniques_csv, header_communes_uniques_csv_v2_2, data_communes_uniques_csv_v2_2)
+ecriture_csv(data_communes_uniques_csv, header_communes_uniques_csv_v2_2,
+             data_communes_uniques_csv_v2_2)
 lecture_csv(data_communes_uniques_csv)
 space_graph(2)
 
-#Position des en-tête codeInstallation = 4, codeInseeCommune = 2, libelle = 9, metro = 19, bus = 20, tram = 21, train = 22,
-# bateau = 23, autreTransport = 24, aucunTransport = 25, dateMAJ = 26, dateCreation = 27
-#header_admissions_par_formation_detaillee_csv_v2 = [(csv_en_liste_v2(data_csv, 0, 4)), (csv_en_liste_v2(data_csv, 0, 2)), (csv_en_liste_v2(data_csv, 0, 9)), (csv_en_liste_v2(data_csv, 0, 19)), (csv_en_liste_v2(data_csv, 0, 20)), (csv_en_liste_v2(data_csv, 0, 21)), (csv_en_liste_v2(data_csv, 0, 22)), (csv_en_liste_v2(data_csv, 0, 23)), (csv_en_liste_v2(data_csv, 0, 24)), (csv_en_liste_v2(data_csv, 0, 25)), (csv_en_liste_v2(data_csv, 0, 26)), (csv_en_liste_v2(data_csv, 0, 27))]
-#data_admissions_par_formation_detaillee_csv_v2 = [(csv_en_liste_v2(data_csv, 1, 4)), (csv_en_liste_v2(data_csv, 1, 2)), (csv_en_liste_v2(data_csv, 1, 9)), (csv_en_liste_v2(data_csv, 1, 19)), (csv_en_liste_v2(data_csv, 1, 20)), (csv_en_liste_v2(data_csv, 1, 21)), (csv_en_liste_v2(data_csv, 1, 22)), (csv_en_liste_v2(data_csv, 1, 23)), (csv_en_liste_v2(data_csv, 1, 24)), (csv_en_liste_v2(data_csv, 1, 25)), (csv_en_liste_v2(data_csv, 1, 26)), (csv_en_liste_v2(data_csv, 1, 27))]
-
-header_admissions_par_formation_detaillee_csv_v2_2 = [
-    (csv_en_liste_v2(data_csv, 0, codeInstallation)), (csv_en_liste_v2(data_csv, 0, codeInseeCommune)),
-    (csv_en_liste_v2(data_csv, 0, libelle)), (csv_en_liste_v2(data_csv, 0, metro)),
-    (csv_en_liste_v2(data_csv, 0, bus)), (csv_en_liste_v2(data_csv, 0, tram)),
-    (csv_en_liste_v2(data_csv, 0, train)), (csv_en_liste_v2(data_csv, 0, bateau)),
-    (csv_en_liste_v2(data_csv, 0, autreTransport)), (csv_en_liste_v2(data_csv, 0, aucunTransport)),
-    (csv_en_liste_v2(data_csv, 0, dateMAJ)), (csv_en_liste_v2(data_csv, 0, dateCreation))
-]
-data_admissions_par_formation_detaillee_csv_v2_2 = [
-    (csv_en_liste_v2(data_csv, 1, codeInstallation)), (csv_en_liste_v2(data_csv, 1, codeInseeCommune)),
-    (csv_en_liste_v2(data_csv, 1, libelle)), (csv_en_liste_v2(data_csv, 1, metro)),
-    (csv_en_liste_v2(data_csv, 1, bus)), (csv_en_liste_v2(data_csv, 1, tram)),
-    (csv_en_liste_v2(data_csv, 1, train)), (csv_en_liste_v2(data_csv, 1, bateau)),
-    (csv_en_liste_v2(data_csv, 1, autreTransport)), (csv_en_liste_v2(data_csv, 1, aucunTransport)),
-    (csv_en_liste_v2(data_csv, 1, dateMAJ)), (csv_en_liste_v2(data_csv, 1, dateCreation))
-]
-
-ecriture_csv(data_admissions_par_formation_detaillee_csv, header_admissions_par_formation_detaillee_csv_v2_2, data_admissions_par_formation_detaillee_csv_v2_2)
+ecriture_csv(data_admissions_par_formation_detaillee_csv,
+             header_admissions_par_formation_detaillee_csv_v2_2,
+             data_admissions_par_formation_detaillee_csv_v2_2)
 lecture_csv(data_admissions_par_formation_detaillee_csv)
 space_graph(4)
 
 # ----  Test de la fonction fichier.csv en liste ----
 
-ligne = 5  # ligne 0 = en-tête
-valeur = 3  # valeur = A sur le execel
-csv_en_liste(data_csv, ligne, valeur)
+ligne = 1  # ligne 0 = en-tête
+valeur = 0  # valeur = A sur le execel
+print("la valeur de la colone", valeur, "est:",
+      csv_en_liste_v2(data_csv, 0, valeur))
+print("la valeur de la ligne", ligne, "est :",
+      csv_en_liste_v2(data_csv, ligne, valeur))
+print("la valeur de", csv_en_liste_v2(data_csv, 0, valeur), "est :",
+      csv_en_liste_v2(data_csv, ligne, valeur))
 space_graph(1)
--- main_test_SC.py
+++ main_test_SC.py
@@ -15,9 +15,35 @@ data_admissions_par_formation_detaillee_csv = "data/AdmissionParFormationDetaill
 #  ------ Diconnaire de recherche ------
 
 dico = {
-    "a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9, "k": 10,
-        "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18, "t": 19, "u" :20,
-        "v": 21, "w": 22, "x": 23, "y": 24, "z": 25, "aa": 26, "ab": 27, "ac": 28
+  "a": 0,
+  "b": 1,
+  "c": 2,
+  "d": 3,
+  "e": 4,
+  "f": 5,
+  "g": 6,
+  "h": 7,
+  "i": 8,
+  "j": 9,
+  "k": 10,
+  "l": 11,
+  "m": 12,
+  "n": 13,
+  "o": 14,
+  "p": 15,
+  "q": 16,
+  "r": 17,
+  "s": 18,
+  "t": 19,
+  "u": 20,
+  "v": 21,
+  "w": 22,
+  "x": 23,
+  "y": 24,
+  "z": 25,
+  "aa": 26,
+  "ab": 27,
+  "ac": 28
 }
 
 # ---- requete de recherche dans le dico ----
@@ -56,23 +82,27 @@ nbEquipements = dico.get("ac")
 
 # ----- Fonction esthétique / grap -----
 
+
 # fonction espace
 def space_graph(valeur):
-    for i in range(valeur):
-        print("")
+  for i in range(valeur):
+    print("")
+
 
 ######    Lecutre fichier CSV    ######
 
+
 # fonction lecture de fichier CSV avec en paramètres le nom du fichier.csv
 def lecture_csv(nom_fichier_csv):
-    # Ouverture d'un fichier csv avec en paramètres un mode "r" read = lecture
-    with open(nom_fichier_csv, errors="ignore") as csv_file:
-        # Lecture du fichier csv
-        csv_reader = csv.reader(csv_file)
-        # Boucle pour lire le fichier csv
-        for line in csv_reader:
-            # Affichage de la ligne lu
-            print(line)
+  # Ouverture d'un fichier csv avec en paramètres un mode "r" read = lecture
+  with open(nom_fichier_csv, errors="ignore") as csv_file:
+    # Lecture du fichier csv
+    csv_reader = csv.reader(csv_file)
+    # Boucle pour lire le fichier csv
+    for line in csv_reader:
+      # Affichage de la ligne lu
+      print(line)
+
 
 lecture_csv(data_csv)
 
@@ -91,15 +121,17 @@ def ecriture_csv(nom_fichier_csv, header):
         writer.writerow(["31", "Haute-Garonne"]) # test ecriture dans le fichier csv
 """
 """ fonction écriture V2 """
+
+
 def ecriture_csv(nom_fichier_csv, header, data):
-    # Ouverutre d'un fichier csv en mode 'w" write = écriture et un saut de ligne
-    with open(nom_fichier_csv, "w", newline="") as csv_file:
-        # Écriture du fichier csv avec un paramètre un delimitation par le ";"
-        writer = csv.writer(csv_file, delimiter=";")
-        # Écriture de l'en tête du fichier csv
-        writer.writerow(header)
-        # Écriture du fichier.csv
-        writer.writerow(data)  # test ecriture dans le fichier csv
+  # Ouverutre d'un fichier csv en mode 'w" write = écriture et un saut de ligne
+  with open(nom_fichier_csv, "w", newline="") as csv_file:
+    # Écriture du fichier csv avec un paramètre un delimitation par le ";"
+    writer = csv.writer(csv_file, delimiter=";")
+    # Écriture de l'en tête du fichier csv
+    writer.writerow(header)
+    # Écriture du fichier.csv
+    writer.writerow(data)  # test ecriture dans le fichier csv
 
 
 # ----- Csv to Liste ------
@@ -117,16 +149,16 @@ def csv_en_liste(nom_fichier_csv, valeur_ligne, valeur_valeur):
     #print(f"Le nombre de lignes du fichier: {len(lignes)}.")
 """
 
-def csv_en_liste_v2(nom_fichier_csv, valeur_ligne, valeur_valeur):
-    with open(nom_fichier_csv, errors="ignore") as f:
-        lecture = csv.reader(f, delimiter=";")
-        lignes = list(lecture)
 
-    return lignes[valeur_ligne][valeur_valeur]
+def csv_en_liste_v2(nom_fichier_csv, valeur_ligne, valeur_valeur):
+  with open(nom_fichier_csv, errors="ignore") as f:
+    lecture = csv.reader(f, delimiter=";")
+    lignes = list(lecture)
+
+  return lignes[valeur_ligne][valeur_valeur]
 
 
 # ------ Appel de fonction -----
-
 """ En-tête V1
 header_installations_uniques_csv =["codeInstallation", "nomInstallation", "nbEquipements", "adresse", "nbParking", "nbParkingHandi"]
 ecriture_csv(data_installations_uniques_csv, header_installations_uniques_csv)
@@ -137,7 +169,6 @@ ecriture_csv(data_communes_uniques_csv, header_communes_uniques_csv)
 header_admissions_par_formation_detaillee_csv = ["codeInstallation", "codeInseeCommune", "libelle", "metro", "bus", "tram", "train", "bateau", "autreTransport", "aucunTransport", "dateMAJ", "dateCreation"]
 ecriture_csv(data_admissions_par_formation_detaillee_csv, header_admissions_par_formation_detaillee_csv)
 """
-
 """ En-tête V2
 header_installations_uniques_csv_v2 = [(csv_en_liste_v2(data_csv, 0, 4)), (csv_en_liste_v2(data_csv, 0, 5)), (csv_en_liste_v2(data_csv, 0, 28)), (csv_en_liste_v2(data_csv, 0, 6)), (csv_en_liste_v2(data_csv, 0, 16)), (csv_en_liste_v2(data_csv, 0, 17))]
 
@@ -145,28 +176,36 @@ header_communes_uniques_csv_v2 = [(csv_en_liste_v2(data_csv, 0, 2)), (csv_en_lis
 
 header_admissions_par_formation_detaillee_csv_v2 = [(csv_en_liste_v2(data_csv, 0, 4)), (csv_en_liste_v2(data_csv, 0, 2)), (csv_en_liste_v2(data_csv, 0, 9)), (csv_en_liste_v2(data_csv, 0, 19)), (csv_en_liste_v2(data_csv, 0, 20)), (csv_en_liste_v2(data_csv, 0, 21)), (csv_en_liste_v2(data_csv, 0, 22)), (csv_en_liste_v2(data_csv, 0, 23)), (csv_en_liste_v2(data_csv, 0, 24)), (csv_en_liste_v2(data_csv, 0, 25)), (csv_en_liste_v2(data_csv, 0, 26)), (csv_en_liste_v2(data_csv, 0, 27))]
 """
-
 """ En-tête V2.2"""
 header_admissions_par_formation_detaillee_csv_v2_2 = [
-    (csv_en_liste_v2(data_csv, 0, codeInstallation)), (csv_en_liste_v2(data_csv, 0, codeInseeCommune)),
-    (csv_en_liste_v2(data_csv, 0, libelle)), (csv_en_liste_v2(data_csv, 0, metro)),
-    (csv_en_liste_v2(data_csv, 0, bus)), (csv_en_liste_v2(data_csv, 0, tram)),
-    (csv_en_liste_v2(data_csv, 0, train)), (csv_en_liste_v2(data_csv, 0, bateau)),
-    (csv_en_liste_v2(data_csv, 0, autreTransport)), (csv_en_liste_v2(data_csv, 0, aucunTransport)),
-    (csv_en_liste_v2(data_csv, 0, dateMAJ)), (csv_en_liste_v2(data_csv, 0, dateCreation))
+  (csv_en_liste_v2(data_csv, 0, codeInstallation)),
+  (csv_en_liste_v2(data_csv, 0,
+                   codeInseeCommune)), (csv_en_liste_v2(data_csv, 0, libelle)),
+  (csv_en_liste_v2(data_csv, 0, metro)), (csv_en_liste_v2(data_csv, 0, bus)),
+  (csv_en_liste_v2(data_csv, 0, tram)), (csv_en_liste_v2(data_csv, 0, train)),
+  (csv_en_liste_v2(data_csv, 0, bateau)),
+  (csv_en_liste_v2(data_csv, 0, autreTransport)),
+  (csv_en_liste_v2(data_csv, 0, aucunTransport)),
+  (csv_en_liste_v2(data_csv, 0, dateMAJ)),
+  (csv_en_liste_v2(data_csv, 0, dateCreation))
 ]
 
-header_communes_uniques_csv_v2_2 = [
-  (csv_en_liste_v2(data_csv, 0, codeInseeCommune)), (csv_en_liste_v2(data_csv, 0, nomCommune)),
-  (csv_en_liste_v2(data_csv, 0, codeDepartement)), (csv_en_liste_v2(data_csv, 0, nomDepartement))
-]
+header_communes_uniques_csv_v2_2 = [(csv_en_liste_v2(data_csv, 0,
+                                                     codeInseeCommune)),
+                                    (csv_en_liste_v2(data_csv, 0, nomCommune)),
+                                    (csv_en_liste_v2(data_csv, 0,
+                                                     codeDepartement)),
+                                    (csv_en_liste_v2(data_csv, 0,
+                                                     nomDepartement))]
 
 header_installations_uniques_csv_v2_2 = [
-  (csv_en_liste_v2(data_csv, 0, codeInstallation)), (csv_en_liste_v2(data_csv, 0, nomInstallation)),
-  (csv_en_liste_v2(data_csv, 0, nbEquipements)), (csv_en_liste_v2(data_csv, 0, adresse)),
-  (csv_en_liste_v2(data_csv, 0, nbParking)), (csv_en_liste_v2(data_csv, 0, nbParkingHandi))
+  (csv_en_liste_v2(data_csv, 0, codeInstallation)),
+  (csv_en_liste_v2(data_csv, 0, nomInstallation)),
+  (csv_en_liste_v2(data_csv, 0, nbEquipements)),
+  (csv_en_liste_v2(data_csv, 0, adresse)),
+  (csv_en_liste_v2(data_csv, 0, nbParking)),
+  (csv_en_liste_v2(data_csv, 0, nbParkingHandi))
 ]
-
 """ Data V2
 data_installations_uniques_csv_v2 = [(csv_en_liste_v2(data_csv, 1, 4)), (csv_en_liste_v2(data_csv, 1, 5)), (csv_en_liste_v2(data_csv, 1, 28)), (csv_en_liste_v2(data_csv, 1, 6)), (csv_en_liste_v2(data_csv, 1, 16)), (csv_en_liste_v2(data_csv, 1, 17))]
 
@@ -174,33 +213,40 @@ data_communes_uniques_csv_v2 = [(csv_en_liste_v2(data_csv, 1, 2)), (csv_en_liste
 
 data_admissions_par_formation_detaillee_csv_v2 = [(csv_en_liste_v2(data_csv, 1, 4)), (csv_en_liste_v2(data_csv, 1, 2)), (csv_en_liste_v2(data_csv, 1, 9)), (csv_en_liste_v2(data_csv, 1, 19)), (csv_en_liste_v2(data_csv, 1, 20)), (csv_en_liste_v2(data_csv, 1, 21)), (csv_en_liste_v2(data_csv, 1, 22)), (csv_en_liste_v2(data_csv, 1, 23)), (csv_en_liste_v2(data_csv, 1, 24)), (csv_en_liste_v2(data_csv, 1, 25)), (csv_en_liste_v2(data_csv, 1, 26)), (csv_en_liste_v2(data_csv, 1, 27))]
 """
-
 """ Data V2.2"""
 data_admissions_par_formation_detaillee_csv_v2_2 = [
-    (csv_en_liste_v2(data_csv, 1, codeInstallation)), (csv_en_liste_v2(data_csv, 1, codeInseeCommune)),
-    (csv_en_liste_v2(data_csv, 1, libelle)), (csv_en_liste_v2(data_csv, 1, metro)),
-    (csv_en_liste_v2(data_csv, 1, bus)), (csv_en_liste_v2(data_csv, 1, tram)),
-    (csv_en_liste_v2(data_csv, 1, train)), (csv_en_liste_v2(data_csv, 1, bateau)),
-    (csv_en_liste_v2(data_csv, 1, autreTransport)), (csv_en_liste_v2(data_csv, 1, aucunTransport)),
-    (csv_en_liste_v2(data_csv, 1, dateMAJ)), (csv_en_liste_v2(data_csv, 1, dateCreation))
+  (csv_en_liste_v2(data_csv, 1, codeInstallation)),
+  (csv_en_liste_v2(data_csv, 1,
+                   codeInseeCommune)), (csv_en_liste_v2(data_csv, 1, libelle)),
+  (csv_en_liste_v2(data_csv, 1, metro)), (csv_en_liste_v2(data_csv, 1, bus)),
+  (csv_en_liste_v2(data_csv, 1, tram)), (csv_en_liste_v2(data_csv, 1, train)),
+  (csv_en_liste_v2(data_csv, 1, bateau)),
+  (csv_en_liste_v2(data_csv, 1, autreTransport)),
+  (csv_en_liste_v2(data_csv, 1, aucunTransport)),
+  (csv_en_liste_v2(data_csv, 1, dateMAJ)),
+  (csv_en_liste_v2(data_csv, 1, dateCreation))
 ]
 
-data_communes_uniques_csv_v2_2 = [
-  (csv_en_liste_v2(data_csv, 1, codeInseeCommune)), (csv_en_liste_v2(data_csv, 1, nomCommune)),
-  (csv_en_liste_v2(data_csv, 1, codeDepartement)), (csv_en_liste_v2(data_csv, 1, nomDepartement))
-]
+data_communes_uniques_csv_v2_2 = [(csv_en_liste_v2(data_csv, 1,
+                                                   codeInseeCommune)),
+                                  (csv_en_liste_v2(data_csv, 1, nomCommune)),
+                                  (csv_en_liste_v2(data_csv, 1,
+                                                   codeDepartement)),
+                                  (csv_en_liste_v2(data_csv, 1,
+                                                   nomDepartement))]
 
 data_installations_uniques_csv_v2_2 = [
-  (csv_en_liste_v2(data_csv, 1, codeInstallation)), (csv_en_liste_v2(data_csv, 1, nomInstallation)),
-  (csv_en_liste_v2(data_csv, 1, nbEquipements)), (csv_en_liste_v2(data_csv, 1, adresse)),
-  (csv_en_liste_v2(data_csv, 1, nbParking)), (csv_en_liste_v2(data_csv, 1, nbParkingHandi))
+  (csv_en_liste_v2(data_csv, 1, codeInstallation)),
+  (csv_en_liste_v2(data_csv, 1, nomInstallation)),
+  (csv_en_liste_v2(data_csv, 1, nbEquipements)),
+  (csv_en_liste_v2(data_csv, 1, adresse)),
+  (csv_en_liste_v2(data_csv, 1, nbParking)),
+  (csv_en_liste_v2(data_csv, 1, nbParkingHandi))
 ]
-
 
 # ------   Test des nouveau fichier csv cree  -----
 
-# -----  test écriture puis lecture  ------ 
-
+# -----  test écriture puis lecture  ------
 """ Test écriture/lecutre v1
 lecture_csv(data_installations_uniques_csv)
 print("")
@@ -208,7 +254,6 @@ lecture_csv(data_communes_uniques_csv)
 print("")
 lecture_csv(data_admissions_par_formation_detaillee_csv)
 """
-
 """ test écriture/lecutre v2
 
 ecriture_csv(data_installations_uniques_csv, header_installations_uniques_csv_v2, data_installations_uniques_csv_v2)
@@ -223,27 +268,40 @@ ecriture_csv(data_admissions_par_formation_detaillee_csv, header_admissions_par_
 lecture_csv(data_admissions_par_formation_detaillee_csv)
 space_graph(4)
 """
-
 """ test écriture/lecutre v2.2 """
 
-ecriture_csv(data_installations_uniques_csv, header_installations_uniques_csv_v2_2, data_installations_uniques_csv_v2_2)
+ecriture_csv(data_installations_uniques_csv,
+             header_installations_uniques_csv_v2_2,
+             data_installations_uniques_csv_v2_2)
 lecture_csv(data_installations_uniques_csv)
 space_graph(2)
 
-ecriture_csv(data_communes_uniques_csv, header_communes_uniques_csv_v2_2, data_communes_uniques_csv_v2_2)
+ecriture_csv(data_communes_uniques_csv, header_communes_uniques_csv_v2_2,
+             data_communes_uniques_csv_v2_2)
 lecture_csv(data_communes_uniques_csv)
 space_graph(2)
 
-ecriture_csv(data_admissions_par_formation_detaillee_csv, header_admissions_par_formation_detaillee_csv_v2_2, data_admissions_par_formation_detaillee_csv_v2_2)
+ecriture_csv(data_admissions_par_formation_detaillee_csv,
+             header_admissions_par_formation_detaillee_csv_v2_2,
+             data_admissions_par_formation_detaillee_csv_v2_2)
 lecture_csv(data_admissions_par_formation_detaillee_csv)
 space_graph(4)
 
-
 #### test csv en liste ###
-
 """ test csv to liste v1 
 ligne = 2 # ligne 0 = en-tête 
 valeur = 3 # valeur = A sur le execel 
 csv_en_liste(data_csv, ligne, valeur)
 """
+""" test csv to liste V2"""
+ligne = 1  # ligne 0 = en-tête
+valeur = 0  # valeur = A sur le execel
+print("la valeur de la colone", valeur, "est:",
+      csv_en_liste_v2(data_csv, 0, valeur))
+print("la valeur de la ligne", ligne, "est :",
+      csv_en_liste_v2(data_csv, ligne, valeur))
+print("la valeur de", csv_en_liste_v2(data_csv, 0, valeur), "est :",
+      csv_en_liste_v2(data_csv, ligne, valeur))
+space_graph(1)
+
 ######    Lecture / Ecriture / csv->liste : ok  avec version  ######
