c662f07c1e0d4cb3833cd8d46c730a0a3b1b5343
Scooter76000 <33722779+scooter76000@users.noreply.github.com>
2023-02-07 13:09:10 +0000

    fonction ecrire  444 ,147, 444

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import csv
 from fonctions.dictionnaire import *
+import time
 
-#  ----- chemin des fichier.csv  ------ 
+#  ----- chemin des fichier.csv  ------
 
 data_csv = "2020_installations_sportives.csv"  # fichier orginal (sujet)
 
@@ -14,16 +15,18 @@ communes_uniques_csv = "data/CommunesUniques.csv"
 # Fichier constituer du code de l'installation, du code INSEE de la commune, du libelle, des different moyen de transport(Metro,Bus,Tram,Train,Bateau ou autre voir pas) et des la date de mise à jour et de création
 admissions_par_formation_detaillee_csv = "data/AdmissionParFormationDetaillee.csv"
 
-
 # ------ Fonction -------
 # ----- Fonction esthétique / grap -----
+
 
 # fonction espace
 def space_graph(valeur):
   for i in range(valeur):
     print("")
 
-
+def P(val, temp):
+  print(val)
+  time.sleep(temp)
 # fonction lecture de fichier CSV avec en paramètres le nom du fichier.csv
 def lecture_csv(nom_fichier_csv):
   # Ouverture d'un fichier csv avec en paramètres un mode "r" read = lecture
@@ -36,39 +39,69 @@ def lecture_csv(nom_fichier_csv):
       print(line)
 
 
+"""fonction lecture et ecriture de fichier CSV qui prend en paramètre le nom du fichier en entrée et celui en sortie une liste d'Index des colonnes qu'on veut recuperer"""
+def lire_et_ecrire_csv(nom_fichier_entree, nom_fichier_sortie,
+                       colonnes_a_recuperer, nombre_ligne):
+  """Ici on lit le fichier csv et on rentre les donn""ées dans une liste"""
+  
+  donnees = [] 
+  with open(nom_fichier_entree, 'r', encoding="ISO-8859-1") as fichier_entree:
+    reader = csv.reader(fichier_entree, delimiter=';')
+    for ligne in reader:
+      donnees.append(ligne)
+        
+  """ici la fonction crée un nouveau fichier avec le nom en paramètre et avec une double boucle une qui récuperer les ligne et l'autre les colonnes la fonction recupére les données de la liste et les ecrit dans le nouveau fichier csv"""
+  with open(nom_fichier_sortie, 'w', newline='') as fichier_sortie:
+    writer = csv.writer(fichier_sortie)
+    ligne_max = 0
+    for ligne in donnees:
+      if ligne_max <= nombre_ligne :
+        colonnes_selectionnees = [
+        ligne[colonne] for colonne in colonnes_a_recuperer
+      ]
+        ligne_max +=1
+        writer.writerow(colonnes_selectionnees)
+  
 
-#fonction ecriture de fichier CSV 
-def lire_et_ecrire_csv(nom_fichier_entree, nom_fichier_sortie, colonnes_a_recuperer):
-    donnees = []
-    with open(nom_fichier_entree, 'r', encoding = "ISO-8859-1") as fichier_entree:
-        reader = csv.reader(fichier_entree, delimiter = ';')
-        for ligne in reader:
-            donnees.append(ligne)
+"""Fonction qui recupére les données d'un fichier csv et les stocks dans une liste avec en paramètre le nom du fichier et le nom de la liste qu'on veut creer"""               
+def changement_de_données (nom_fichier_entree, nom_liste) :
+    nom_liste = []
+    with open(nom_fichier_entree, 'r', encoding="ISO-8859-1") as fichier_entree:
+      reader = csv.reader(fichier_entree, delimiter=';')
+      for ligne in reader:
+        nom_liste.append(ligne)
+      return nom_liste
 
-    with open(nom_fichier_sortie, 'w', newline='') as fichier_sortie:
-        writer = csv.writer(fichier_sortie)
-        for ligne in donnees:
-            colonnes_selectionnees = [ligne[colonne] for colonne in colonnes_a_recuperer]
-            writer.writerow(colonnes_selectionnees)
-          
-            
 #Position des en-tête codeInstallation = 4, nomInstallation = 5, nbEquipements = 28, adresse = 6, nbParking = 16, nbParkingHandi = 17
 
+# --------------   Test ecriture -------
 
-# --------------   Test ecriture -------   
-          
-lire_et_ecrire_csv(data_csv, installations_uniques_csv, [codeInstallation, nomInstallation, nbEquipements, adresse, nbParking, nbParkingHandi])
+lire_et_ecrire_csv(data_csv, installations_uniques_csv, [
+  codeInstallation, nomInstallation, nbEquipements, adresse, nbParking,
+  nbParkingHandi
+], 444)
 #Position des en-tête codeInseeCommune = 2, nomComune = 3, codeDepartement = 0, nomDepartement = 1
-lire_et_ecrire_csv(data_csv, communes_uniques_csv, [codeInseeCommune, nomCommune, codeDepartement, nomDepartement])
+lire_et_ecrire_csv(
+  data_csv, communes_uniques_csv,
+  [codeInseeCommune, nomCommune, codeDepartement, nomDepartement], 146)
 #Position des en-tête codeInstallation = 4, codeInseeCommune = 2, libelle = 9, metro = 19, bus = 20, tram = 21,train = 22, bateau = 23, autreTransport = 24, aucunTransport = 25, dateMAJ = 26, dateCreation = 27
-lire_et_ecrire_csv(data_csv, admissions_par_formation_detaillee_csv, [codeInstallation, codeInseeCommune, libelle, metro, bus, tram, train, bateau, autreTransport, aucunTransport, dateMAJ, dateCreation])
-
+lire_et_ecrire_csv(data_csv, admissions_par_formation_detaillee_csv, [
+  codeInstallation, codeInseeCommune, libelle, metro, bus, tram, train, bateau,
+  autreTransport, aucunTransport, dateMAJ, dateCreation
+], 444)
 
 # ------ Test Lecture  -------------
-"""
+P("Chagement de fichier csv Admission", 2)
 lecture_csv(admissions_par_formation_detaillee_csv)
 space_graph(4)
+
+time.sleep(2)
+P("Chagement de fichier csv Communes", 4)
 lecture_csv(communes_uniques_csv)
 space_graph(4)
+
+time.sleep(2)
+P("Chagement de fichier csv Installations", 4)
 lecture_csv(installations_uniques_csv)
-"""
+space_graph(4)
+print("Chagement des fichiers csv terminer")
