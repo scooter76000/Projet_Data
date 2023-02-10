13a25918c42495269bee217c995bdce6ed820bbe
Scooter76000 <33722779+scooter76000@users.noreply.github.com>
2023-02-10 10:11:23 +0000

    fonction à remplir

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dictionnaire import *
 
 data_csv = "2020_installations_sportives.csv"  # fichier orginal (sujet)
 
-# Fichier constituer du code de l'installation, nom de l'installation, nombre d'Equipement, de l'adresse et du nombre de place de parking handicaper ou pas.
+# Fichier constitué du code de l'installation, nom de l'installation, nombre d'Equipement, de l'adresse et du nombre de place de parking handicaper ou pas.
 installations_uniques_csv = "data/IntallationsUniques.csv"
 
 # Fichier constituer du code INSEE de la commune, du nom de la commune, du code du department et du nom du département.
@@ -28,7 +28,15 @@ def chargment(valeur, temps):
 
 # ----  Fonction fonctionnelle ----- 
 
+"""
+# Fonction : exemple de fonction
+# parametre : nom_fichier_csv, str
+# retourne : S
+"""
+
+
 # Script 1 : Etape 1
+
 
 # fonction lecture de fichier CSV avec en paramètres le nom du fichier.csv
 def lecture_csv(nom_fichier_csv):
@@ -41,7 +49,9 @@ def lecture_csv(nom_fichier_csv):
             # Affichage de la ligne lu
             print(line)
 
+          
 # Script 1 : Etape 2
+
 
 """
 fonction lecture et ecriture de fichier CSV qui prend en paramètre le nom du fichier en entrée et celui en sortie une liste d'Index des colonnes qu'on veut recuperer
@@ -72,20 +82,21 @@ def lire_et_ecrire_csv(nom_fichier_entree, nom_fichier_sortie, colonnes_a_recupe
                 ligne_max += 1
                 writer.writerow(colonnes_selectionnees)
 
+              
 # Script 2 : Etape 3
 
 """
 Fonction qui recupére les données d'un fichier csv et les stocks dans une liste avec en paramètre le nom du fichier et le nom de la liste qu'on veut creer
-""" 
-
-def changement_de_données (nom_fichier_entree, Si) :
-    Si = []
+"""
+def chargement_de_données (nom_fichier_entree) :
+    S = []
     with open(nom_fichier_entree, 'r', encoding="ISO-8859-1") as fichier_entree:
         reader = csv.reader(fichier_entree, delimiter=';')
         for ligne in reader:
-            Si.append(ligne)
-        return Si
+            S.append(ligne)
+        return S
 
+      
 # Script 2 : Etape 4
 
 def projection_simple (liste_entrée, nomDeColonne) :
@@ -96,6 +107,7 @@ def projection_simple (liste_entrée, nomDeColonne) :
         index_colonne = colonne
     S.append(liste_entrée[ligne][index_colonne])      
   print (S)
+  return(S)
 
 
 def projection_simple_distinct(liste_entrée, nomDeColonne) :
@@ -107,9 +119,142 @@ def projection_simple_distinct(liste_entrée, nomDeColonne) :
     if liste_entrée[ligne][index_colonne] not in S :
       S.append(liste_entrée[ligne][index_colonne])      
   print (S)
+  return(S)  
+  
+
+def projection_multiple(liste_entrée,liste_nom_colonnes):
+  S=[[0]*len(liste_nom_colonnes)]*len(liste_entrée)
+  for i in range(len(liste_nom_colonnes)):
+    liste=projection_simple(liste_entrée, liste_nom_colonnes[i])
+    for j in range(len(liste)):
+      S[j][i]=liste[j]
+  print (S)
 
 
+def projection_multiple_distinct(liste_entrée,liste_nom_colonnes):
+  S=[[0]*len(liste_nom_colonnes)]*len(liste_entrée)
+  for i in range(len(liste_nom_colonnes)):
+    liste=projection_simple_distinct(liste_entrée, liste_nom_colonnes[i])
+    for j in range(len(liste)):
+      S[j][i]=liste[j]
+  print (S)
 
+
+    
+S1=chargement_de_données(installations_uniques_csv)
+S2=chargement_de_données(communes_uniques_csv)
+S3=chargement_de_données(admissions_par_formation_detaillee_csv)
+
+
+#Script 2 : Etape 5
+
+
+def sélection_simple(liste_entrée, nomDeColonne, opérateur, valeur):
+  S = []
+  ...
+  return S
+
+
+def sélection_multiple (liste_entrée, operateurbooléen):
+  S = []
+  ...
+  return S
+
+
+#Script 2 : Etape 6
+
+
+def min (liste_entrée, nomDeColonne) :
+  S = []
+  for ligne in range(len(liste_entrée)) :
+    for colonne in range (len(liste_entrée[ligne])) :
+      if liste_entrée[ligne][colonne] == nomDeColonne :
+        index_colonne = colonne
+    S.append(liste_entrée[ligne][index_colonne]) 
+  min = S[1] 
+  for elem in S :
+    if min >= elem :
+      min = elem 
+  return min
+
+
+def max (liste_entrée, nomDeColonne) :
+  S = []
+  for ligne in range(len(liste_entrée)) :
+    for colonne in range (len(liste_entrée[ligne])) :
+      if liste_entrée[ligne][colonne] == nomDeColonne :
+        index_colonne = colonne
+    S.append(liste_entrée[ligne][index_colonne]) 
+  max = S[1]
+  for elem in range(1, len(S)):
+    if max <= S[elem] :
+      max = S[elem]
+  return max
+
+
+def compte (liste_entrée, nomDeColonne) :
+  S = []
+  for ligne in range(len(liste_entrée)) :
+    for colonne in range (len(liste_entrée[ligne])) :
+      if liste_entrée[ligne][colonne] == nomDeColonne :
+        index_colonne = colonne
+    S.append(liste_entrée[ligne][index_colonne]) 
+  Compteur = 0
+  for elem in range(1, len(S)-1):
+    if S[elem] != 0 :
+      Compteur += 1
+  return Compteur
+
+
+def somme (liste_entrée, nomDeColonne) :
+  S = []
+  for ligne in range(len(liste_entrée)) :
+    for colonne in range (len(liste_entrée[ligne])) :
+      if liste_entrée[ligne][colonne] == nomDeColonne :
+        index_colonne = colonne
+    S.append(liste_entrée[ligne][index_colonne]) 
+  Somme = 0
+  for elem in range(1, len(S)):
+    Somme += int(S[elem])
+  return Somme
+
+
+def moyenne (liste_entrée, nomDeColonne) :
+  S = []
+  for ligne in range(len(liste_entrée)) :
+    for colonne in range (len(liste_entrée[ligne])) :
+      if liste_entrée[ligne][colonne] == nomDeColonne :
+        index_colonne = colonne
+    S.append(liste_entrée[ligne][index_colonne]) 
+  Somme = 0
+  for elem in range(1, len(S)):
+    Somme += int(S[elem])
+  Moyenne = Somme/(len(S)-1)
+  return Moyenne
+
+  
+#Script 2 : Etape 7
+
+def ordonne(liste_entrée, nomDeColonne, sens):
+  S = []
+  ...
+  return S
+
+
+def ordonne_multiple(liste_entrée, liste_nom_colonnes, sens):
+  S = []
+  ...
+  return S
+
+
+#Script 2 : Etape 8 
+
+def jointure(liste_entrée, liste_sortie, nomDeColonne):
+  S = []
+  ...
+  return S
+  
+  
 # --------------   Test ecriture -------
 
 """
@@ -128,7 +273,7 @@ lire_et_ecrire_csv(data_csv, admissions_par_formation_detaillee_csv, [
 ], 444)
 """
 
-# ------ Test Lecture  -------------
+# ------ Test Lecture -------------
 
 """
 chargment("Chargement de fichier csv Admission", 2)
@@ -151,3 +296,4 @@ space_graph(4)
 # --------------   Test changmenent des données -------
 
 #changement_de_données(installations_uniques_csv, S1))
+
